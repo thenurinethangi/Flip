5f38f14eeef240c513b2294cce79e7738e2baa3a
thenurinethangi <thenurinathangi@gmail.com>
2026-02-07 00:04:59 +0530

    complete add,remove,update notifications for subtasks

--- services/notificationService.ts
+++ services/notificationService.ts
@@ -1,93 +1,61 @@
 import * as Notifications from "expo-notifications";
 
 export async function scheduleLocalNotificationForCountdown(
-  title: string,
-  body: string,
-  date: Date,
-  id: string,
+    title: string,
+    body: string,
+    date: Date,
+    id: string,
 ) {
-  console.log(title);
-  console.log(body);
-  console.log(date);
+    console.log(title);
+    console.log(body);
+    console.log(date);
 
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title,
-      body,
-      data: {
-        type: "countdown",
-        countdownId: id,
-        action: "OPEN_TASK",
-      },
-    },
-    trigger: {
-      type: Notifications.SchedulableTriggerInputTypes.DATE,
-      date: new Date(Date.now() + 5000),
-    },
-  });
-
-  const notificationId = await Notifications.scheduleNotificationAsync({
-    content: {
-      title,
-      body,
-      data: {
-        type: "countdown",
-        countdownId: id,
-        action: "OPEN_TASK",
-      },
-    },
-    trigger: {
-      type: Notifications.SchedulableTriggerInputTypes.DATE,
-      date,
-    },
-  });
-  return notificationId;
+    const notificationId = await Notifications.scheduleNotificationAsync({
+        content: {
+            title,
+            body,
+            data: {
+                type: "countdown",
+                countdownId: id,
+                action: "OPEN_TASK",
+            },
+        },
+        trigger: {
+            type: Notifications.SchedulableTriggerInputTypes.DATE,
+            date,
+        },
+    });
+    return notificationId;
 }
 
 export async function cancelLocalNotification(id: string) {
-  await Notifications.cancelScheduledNotificationAsync(id);
+    await Notifications.cancelScheduledNotificationAsync(id);
 }
 
 export async function scheduleLocalNotificationForTask(
-  title: string,
-  body: string,
-  date: Date,
-  id: string,
+    title: string,
+    body: string,
+    date: Date,
+    id: string,
 ) {
-  console.log(title);
-  console.log(body);
-  console.log(date);
+    console.log(title);
+    console.log(body);
+    console.log(date);
 
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title,
-      body,
-      data: {
-        type: "task",
-        taskId: id,
-        action: "OPEN_TASK",
-      },
-    },
-    trigger: {
-      type: Notifications.SchedulableTriggerInputTypes.DATE,
-      date: new Date(Date.now() + 5000),
-    },
-  });
-
-  const notificationId = await Notifications.scheduleNotificationAsync({
-    content: {
-      title,
-      body,
-      data: {
-        type: "task",
-        taskId: id,
-        action: "OPEN_TASK",
-      },
-    },
-    trigger: {
-      type: Notifications.SchedulableTriggerInputTypes.DATE,
-      date,
-    },
-  });
-  return notificationId;
+    const notificationId = await Notifications.scheduleNotificationAsync({
+        content: {
+            title,
+            body,
+            data: {
+                type: "task",
+                taskId: id,
+                action: "OPEN_TASK",
+            },
+        },
+        trigger: {
+            type: Notifications.SchedulableTriggerInputTypes.DATE,
+            date,
+        },
+    });
+    return notificationId;
 }
